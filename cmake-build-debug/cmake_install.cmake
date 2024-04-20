0da744adf6cd96a28e846008c7e490a09ea28426
ILYJNY <crimssel@gmail.com>
2024-04-20 13:49:16 +0900

    Sat Apr 20 13:49:16     2024 Up-to-Date

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jeonl/CLionProjects/Reimann Zeta Fun
+# Install script for directory: C:/Users/jeonl/CLionProjects/Reimann_Zeta_Fun
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jeonl/CLionProjects/Reimann Zeta Fun/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jeonl/CLionProjects/Reimann_Zeta_Fun/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
