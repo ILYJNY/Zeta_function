38c448a83d1992b337d0ac036efddd40c9489c2e
ILYJNY <crimssel@gmail.com>
2024-04-20 14:12:02 +0900

    Sat Apr 20 14:12:02     2024 Up-to-Date

--- github_save.sh
+++ github_save.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git init
 git add .
+setted_name="ILYJNY"
 echo "Enter the commit(if you want automatically, type \"Auto\")"
 read input_commit
 if [ ${input_commit} == "Auto" ] ; then
@@ -8,9 +9,32 @@ if [ ${input_commit} == "Auto" ] ; then
 else
   git commit -m "${input_commit}"
 fi
-echo "Enter the git repos"
-read input_repos
+
+echo "Type a type(repos or project and username)"
+read choice
+
+if [ ${choice} == "repos" ] ; then
+
+  echo "Enter the git repos"
+  read input_repos
+
+else
+
+  echo "Enter the User name(if you are setted, type \"Auto\""
+  read input_username
+  if [ ${input_username} == "Auto" ] ; then
+    input_username=${setted_name}
+  else
+    pass
+
+  fi
+  echo "Enter the Project name"
+  read project_name
+  input_repos=https://github.com/${input_username}/${project_name}.git
+
+
+fi
 git remote remove origin
-git remote add origin "${input_repos}"
+git remote add ${input_repos}
 git push origin +master
 pause
